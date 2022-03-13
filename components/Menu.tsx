f2dad1e18df870aa7711143577dbefaa2e8fe61f
fajry hamzah <fhaji.jakmall@gmail.com>
2022-03-13 07:42:33 +0700

    feat: add basic navbar component

---
+++ components/Menu.tsx
@@ -0,0 +1,11 @@
+const NavBar = () => {
+    return <div className="grid grid-cols-3 text-center p-2 text-xl border-solid border-b border-sky-500">
+        <div>
+        <h1><a href="https://fhaji.dev" target="_blank">fhaji.dev</a></h1>
+        </div>
+        <h1>Worclue</h1>
+        <div>Menu</div>
+  </div>;
+};
+
+export default NavBar;
